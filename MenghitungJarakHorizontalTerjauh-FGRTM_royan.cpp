0c2a48355cb3058ca11bf79ddc5a12e46a9dfd30
royanakilah21 <143056619+royanakilah21@users.noreply.github.com>
2023-10-10 09:48:27 +0700

    Add files via upload

---
+++ MenghitungJarakHorizontalTerjauh-FGRTM_royan.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    double v;
+    double s;
+    double jarak;
+
+    cin >> v;
+    cin >> s;
+    jarak = sin(200 * s * 3.141 / 1.8 * 100) * 100 / 4.73;
+    cout << jarak << endl;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
